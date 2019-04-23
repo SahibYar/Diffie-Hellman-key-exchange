bd439fa4a2cbf5125bca97b5051d314781fb6e6a
Sahib Yar <sahibyaar@hotmail.com>
2019-04-23 11:45:03 +0500

    Update Cipher.h

--- DeffieHellman/Header Files/Cipher.h
+++ DeffieHellman/Header Files/Cipher.h
@@ -1,13 +1,18 @@
 #pragma once
 #include <string>
+#include "sha1.h"
+#include "RC4.h"
 
 namespace cipher
 {
 	class DeffieHellman
 	{
 	public:
-		static std::string encrypt(std::string _inputStr, int _offset);
-		static std::string decrypt(std::string _inputStr, int _offset);
-		static char bound(char _charToCheck, char _left, char _right);
+		static std::string encrypt(std::string _inputStr, std::string pasword);
+		static std::string decrypt(std::string _inputStr, std::string pasword);
+		static std::string SHA1(const std::string &string)
+		{
+			return sha1(string);
+		}
 	};
 }
