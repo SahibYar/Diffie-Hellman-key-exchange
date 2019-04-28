f6131e7bdb18b4adf8ee9feee3c8b7edd0a345f8
Sahib Yar <sahibyaar@hotmail.com>
2019-04-28 16:02:37 +0500

    Update Server.cpp

--- Server/Source Files/Server.cpp
+++ Server/Source Files/Server.cpp
@@ -1,149 +1,166 @@
-#include "Server.h"
-#include "../DeffieHellman/Cipher.h"
+#include <iostream>
+#include <process.h>
+#include <random>
+#include <conio.h>
+#include "ServerClass.h"
 
-void* get_in_addr(SOCKADDR* sa) {
-	if (sa->sa_family == AF_INET) {
-		return &(((SOCKADDR_IN*)sa)->sin_addr);
-	}
-	return &(((SOCKADDR_IN6*)sa)->sin6_addr);
-}
-
-void Server::CreateServer() {
-	//get us a socket and bind it
-	ZeroMemory(&hints, sizeof(hints));
-
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_DGRAM;
-	hints.ai_flags = IPPROTO_UDP;	//use my IP
-	hints.ai_protocol = 0;
-
-	if ((returnedValue = getaddrinfo(LOCALHOST, PORT, &hints, &addressInfo)) != 0)
-	{
-		printf("selectserver: %ls\n", gai_strerror(returnedValue));
-		exit(1);
-	}
-
-	for (point = addressInfo; point != NULL; point = point->ai_next) 
-	{
-		sockfd = socket(point->ai_family, point->ai_socktype, point->ai_protocol);
-		if (sockfd == -1) {
-			printf("listener: socket");
-			continue;
-		}
-
-		if (bind(sockfd, point->ai_addr, point->ai_addrlen) == -1) {
-			closesocket(sockfd);
-			printf("listener: bind");
-			continue;
-		}
-		break;
-	}
-	//if we got here, it means we didn't get bound
-	if (point == NULL) 
-	{
-		printf("selectserver: failed to bind\n");
-		exit(2);
-	}
-	freeaddrinfo(addressInfo); //all done with this
-	remoteaddrlen = sizeof(remoteaddr);
-
-	printf("We are NOW running!\n");
-}
-
-void Server::UpdateRecv() 
+int Menu()
 {
-	while (isServerRunning) 
-	{
+	int option; //define option variable
+	system("cls"); //clear the screen 
+	std::cout << "\t\t\t<---::Menu::--->" << std::endl;
+	std::cout << "\t\t\t _________________   " << std::endl;
+	std::cout << "\t\t\t| 1. Start Server |  " << std::endl;
+	std::cout << "\t\t\t| 2. About        |  " << std::endl;
+	std::cout << "\t\t\t| 3. Help         |  " << std::endl;
+	std::cout << "\t\t\t| 4. Quit         |  " << std::endl;
+	std::cout << "\t\t\t|_________________|  " << std::endl;
+	std::cout << "\t\t\tOption: ";
+	std::cin >> option;  //take the input of option
+	std::cin.ignore();
 
-		if ((nbytes = recvfrom(sockfd, buf, MAXDATASIZE - 1, 0, (SOCKADDR*)&remoteaddr, &remoteaddrlen)) == -1) 
-		{
-			return;
-		}
-		if (nbytes > 0) {
-			HandleMessage();
-		}
-	}
+	return option;
 }
 
-void Server::UpdateSend() {
-	std::string message;
-	while (isServerRunning) {
-		getline(std::cin, message);
-		if (strcmp(message.c_str(), "/end") == 0) 
+void CommandHandle(NetworkInit& a);
+void CommandHandle(NetworkInit& a)
+{
+	char command[20] = { 0 };
+
+	std::cin.ignore();
+
+	while (1)
+	{
+		if (kbhit())
 		{
-			//Send end message to all users
-			BroadcastMessageToAll(std::string("4:Serva:Server has been shut down!"));
-			//Shut the server down
-			isServerRunning = false;
-			return;
-		}
-		else {
-			BroadcastMessageToAll(std::string("1:Serva:") + message);
-		}
-	}
-}
+			std::cin.getline(command, 20);
+			if (strcmp(command, "X") == 0)
+			{
+				std::cout << "Host Generating (g^x mod p) and send to client..\n" << std::endl;
 
-void Server::HandleMessage() {
-	//handle data from a client
-	//decrypt buf
-	std::string decryptedStr = cipher::DeffieHellman::decrypt(std::string(buf), -3);
-	char code = decryptedStr[0];
+				unsigned int gXModP = 0;
+				while (gXModP == 0)
+				{
+					std::random_device dev;
+					std::mt19937 rng(dev());
+					std::uniform_int_distribution<std::mt19937::result_type> distP(2, 10); // distribution in range
+					X = distP(rng);
+					gXModP = pow(G, X);
+				}
 
-	if (code == '0') //client is logging on
-	{	
-		printf("Client joined.\n");
+				gXModP = gXModP / P;
 
-		//Add client's address to the vector
-		clientsVec.push_back(*(SOCKADDR*)&remoteaddr);
-		
-		printf("clients size: %i\n", int(clientsVec.size()));
-	}
-	else if (code == '1')  //client is sending a message
-	{
-	   //printf("listener: packet is %d bytes long\n", nbytes);
-		decryptedStr[nbytes] = '\0';
-	
-		printf("(%s): %s\n", inet_ntop(remoteaddr.ss_family, get_in_addr((SOCKADDR*)&remoteaddr), remoteIP, sizeof(remoteIP)), decryptedStr.c_str());
-		//send message to all other clients
-		BroadcastMessageToAll(decryptedStr);
-	}
-	else if (code == '2')  //client is logging out
-	{
-		printf("Client left.\n");
-		for (size_t i = 0; i < clientsVec.size(); i++) {
-			if (CompareClients(&clientsVec[i], (SOCKADDR*)&remoteaddr)) {
-				printf("Going to erase\n");
-				clientsVec.erase(clientsVec.begin() + i);
-				break;
+				std::string s = std::to_string(gXModP);
+				s.append("X");
+				strcpy(command, s.c_str());
+
 			}
+
+			//if (SharedKeyGenerated)
+			//{
+			//	std::string H = K.append(command);
+			//	H = cipher::DeffieHellman::SHA1(H);
+			//	std::string C = std::string(command).append(H);
+			//	encryptedStr = cipher::DeffieHellman::encrypt(std::string(C), std::to_string(PW));
+			//	std::cout << "\nEncrypted Msg:\t\t" << encryptedStr << std::endl;
+			//}
+			//else
+			//{
+			std::string encryptedStr = cipher::DeffieHellman::encrypt(std::string(command), std::to_string(PW));
+			std::cout << "\nEncrypted Msg:\t\t" << encryptedStr << std::endl;
+			//}
+			send(ClientConn, encryptedStr.c_str(), sizeof(encryptedStr), 0);
 		}
-		printf("clients size: %i\n", int(clientsVec.size()));
-		//shut down server if everyone leaves
-		if (clientsVec.size() == 0) {
-			return;
+		if (!strcmp(command, "quit"))
+		{
+			a.~NetworkInit();
+			break;
 		}
 	}
 }
 
-void Server::BroadcastMessage(std::string _message, SOCKADDR* _destination) {
-	remoteaddrlen = sizeof(*_destination);
-	sendto(sockfd, _message.c_str(), _message.length(), 0, _destination, remoteaddrlen);
+void RunServer();
+
+void RunServer()
+{
+	std::fstream file;
+	file.open("../UDPServer/Gen.txt", std::ios::in);
+	file >> P;
+	file >> G;
+	file >> PW;
+	file.close();
+
+	char IP[16];
+	u_short PORT;
+	std::thread CommandHandler;
+
+	system("cls");
+	std::cout << "P, G, and Password loaded from Gen.txt: \n";
+	std::cout << "P= " << P << std::endl;
+	std::cout << "G= " << G << std::endl;
+	std::cout << "PW= " << PW << std::endl;
+
+
+	std::cout << "\n\n\t\tEnter the server ip address: ";
+	std::cin >> IP;
+	std::cout << std::endl;
+
+	std::cout << "\t\tEnter the server port: ";
+	std::cin >> PORT;
+	std::cout << std::endl;
+
+
+	NetworkInit com(IP, PORT);
+
+	CommandHandler = std::thread(CommandHandle, std::ref(com));
+	com.AcceptConn();
 }
 
-void Server::BroadcastMessageToAll(std::string _message) {
-	for (size_t i = 0; i < clientsVec.size(); i++) {
-		//disable the if check if you want to test on localhost
-		// if(!CompareClients(&clientsVec[i], (struct sockaddr *)&remoteaddr)){
-		BroadcastMessage(_message, &clientsVec[i]);
-		// }
-	}
+void about();
+
+void about()
+{
+	system("cls");
+	std::cout << "Made By: Gordon C" << std::endl;
+	std::cout << "UDP Chat Server" << std::endl;
+	system("pause");
 }
 
-bool Server::CompareClients(SOCKADDR* c_1, SOCKADDR* c_2) {
-	return (inet_ntop(remoteaddr.ss_family, get_in_addr(c_1), remoteIP, sizeof remoteIP) ==
-		inet_ntop(remoteaddr.ss_family, get_in_addr(c_2), remoteIP, sizeof remoteIP));
-}
+void help();
 
-void Server::CloseServer() {
-	closesocket(sockfd);
+void help()
+{
+	system("cls");
+	std::cout << "Press 1 to start server" << std::endl;
+	std::cout << "Then enter the ip address on which the server should be accessible...it should be your PC's local ip or 127.0.0.1 for localhost" << std::endl;
+	std::cout << "After that enter the port on which it should work" << std::endl;
+	std::cout << "Now the Server is setup and now clients can connect" << std::endl;
+	std::cout << "Check the documentation for more details..." << std::endl;
+	system("pause");
+}
+int main()
+{
+	do
+	{
+		switch (Menu())
+		{
+		case 1:
+			RunServer();
+			break;
+
+		case 2:
+			about();
+			break;
+
+		case 3:
+			help();
+			break;
+
+		case 4:
+			exit(0);
+			break;
+		}
+	} while (1);
+
+	return 0;
 }
